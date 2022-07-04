2b265382822bbdfafde06588fc97bea72770841e
yessica <yessica@leanmind.es>
2022-07-04 11:45:50 +0100

    GREEN: IVA and IGIC taxes fields are exclusives

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -1,5 +1,11 @@
 export const csvFilterCalculator = (invoice: string[]): string[] | string => {
   if (invoice.length > 1) {
+    const invoiceValues = invoice[1].split(',');
+    const ivaField = invoiceValues[4];
+    const igicField = invoiceValues[5];
+    if (ivaField && igicField) {
+      return invoice[0];
+    }
     return invoice;
   }
   return invoice.toString();
