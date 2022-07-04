7b49eb1bb2c3eb3bba7921bc4720ab71e7be9be1
yessica <yessica@leanmind.es>
2022-07-04 15:10:06 +0100

    REFACTOR csvFilterCalculator and csvFilterCalculator.tests

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -1,43 +1,57 @@
-export const csvFilterCalculator = (invoiceEntry: string[]): string[] | string => {
-  const header = invoiceEntry.shift();
-  const listOfInvoices = invoiceEntry;
+const LIST_SEPARATOR = ',';
+
+const divideInvoiceBySections = (invoice: string[]) => {
+  const invoiceHeader = invoice.shift();
+  const listOfInvoices: string[] = invoice;
+  return { invoiceHeader, listOfInvoices };
+}
+
+const listOfUniquesInvoicesIds = (invoicesList: string[]): string[] => {
+  const idsOfValidInvoices = [];
+
+  invoicesList.forEach(element => {
+    const elementId = element.split(LIST_SEPARATOR)[0];
+    idsOfValidInvoices.includes(elementId)
+      ? idsOfValidInvoices.splice(idsOfValidInvoices.indexOf(elementId))
+      : idsOfValidInvoices.push(elementId);
+  });
+  return idsOfValidInvoices;
+}
+
+const ivaAndIgicAreExclusives = (ivaValue: number, igicValue: number): boolean => !(ivaValue && igicValue);
+const cifAndNifAreExclusives = (cifValue: string, nifValue: string): boolean => !(cifValue && nifValue);
+
+const calculateAmountWithTaxes = (amount: number, tax: number): number => amount + (amount * tax / 100);
+
+export const csvFilterCalculator = (baseInvoices: string[]): string[] | string => {
+  const { invoiceHeader, listOfInvoices } = divideInvoiceBySections(baseInvoices);
 
   if (listOfInvoices.length) {
-    const idsOfValidInvoices = [];
+    const validInvoices = [invoiceHeader];
+    const idsOfValidInvoices = listOfUniquesInvoicesIds(listOfInvoices);
 
-    listOfInvoices.forEach(element => {
-      const elementId = element.split(',')[0]
-      if (!idsOfValidInvoices.includes(elementId)) {
-        idsOfValidInvoices.push(element.split(',')[0])
-      } else {
-        idsOfValidInvoices.splice(idsOfValidInvoices.indexOf(elementId))
-      }
-    });
-
-    const validInvoices = [header];
-    listOfInvoices.forEach(invoice => {
-      const invoiceValues = invoice.split(',');
+    listOfInvoices.forEach((invoice: string) => {
+      const invoiceValues = invoice.split(LIST_SEPARATOR);
+      
       if (idsOfValidInvoices.includes(invoiceValues[0])) {
-
-        const ivaField = invoiceValues[4];
-        const igicField = invoiceValues[5];
+        const ivaField = invoiceValues[4] && Number(invoiceValues[4]);
+        const igicField = invoiceValues[5] && Number(invoiceValues[5]);
         const cifField = invoiceValues[7];
         const nifField = invoiceValues[8];
         const totalAmount = Number(invoiceValues[3]);
-        const amountWithoutTaxes = Number(invoiceValues[2])
+        const amountWithoutTaxes = Number(invoiceValues[2]);
 
-        const totalAmountCalculatedWithIva = amountWithoutTaxes + (amountWithoutTaxes * Number(ivaField) / 100);
-        const totalAmountCalculatedWithIgic = amountWithoutTaxes + (amountWithoutTaxes * Number(igicField) / 100);
-        if (!(ivaField && igicField
-          || (cifField && nifField)
-          || (ivaField && totalAmount !== totalAmountCalculatedWithIva)
-          || (igicField && totalAmount !== totalAmountCalculatedWithIgic))
+        const selectedTax = ivaField ? ivaField : igicField;
+
+        if ((ivaAndIgicAreExclusives(ivaField, igicField)
+          && cifAndNifAreExclusives(cifField, nifField))
+          && totalAmount === calculateAmountWithTaxes(amountWithoutTaxes, selectedTax)
         ) {
           validInvoices.push(invoice);
-        } 
+        }
       }
     });
     return validInvoices;
   }
-  return header;
+  return invoiceHeader;
 }
