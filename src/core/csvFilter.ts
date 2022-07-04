2768db8732313d95f41f6cd8703ce7b50e6e12ab
yessica <yessica@leanmind.es>
2022-07-04 12:21:46 +0100

    GREEN: total amount with IGIC is well calculated

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -8,8 +8,13 @@ export const csvFilterCalculator = (invoice: string[]): string[] | string => {
     const totalAmount = Number(invoiceValues[3]);
     const amountWithoutTaxes = Number(invoiceValues[2])
 
-    const totalAmountCalculatedWithIva = amountWithoutTaxes + (Number(amountWithoutTaxes) * Number(ivaField) / 100);
-    if (ivaField && igicField || (cifField && nifField) || (totalAmount !== totalAmountCalculatedWithIva)) {
+    const totalAmountCalculatedWithIva = amountWithoutTaxes + (amountWithoutTaxes * Number(ivaField) / 100);
+    const totalAmountCalculatedWithIgic = amountWithoutTaxes + (amountWithoutTaxes * Number(igicField) / 100);
+    if (ivaField && igicField
+      || (cifField && nifField)
+      || (ivaField && totalAmount !== totalAmountCalculatedWithIva)
+      || (igicField && totalAmount !== totalAmountCalculatedWithIgic)
+    ) {
       return invoice[0];
     }
     return invoice;
